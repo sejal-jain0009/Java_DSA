9c3e65ecb1347ef6fd11764d8f6b0c82098ff939
Sejal jain <sejaljain0009@gmail.com>
2026-02-04 13:22:36 +0530

    sumOfdigits

--- recursion.java
+++ recursion.java
@@ -83,3 +83,16 @@
         return n+sum(n-1);
     }
 }*/
+
+//ques7:sum of digits
+/*class Main {
+    public static void main(String[] args) {
+       System.out.println(digitsum(1342));
+    }
+    
+    public static int digitsum(int n)
+    {
+        if(n==0) return 0;
+        return (n%10)+digitsum(n/10);
+    }
+}*/
