0a69490bfb9c238a36f5aa5a5873d66f6f808627
Sejal jain <sejaljain0009@gmail.com>
2026-02-05 12:34:03 +0530

    countZeroes

--- recursion.java
+++ recursion.java
@@ -192,7 +192,30 @@
     }
 }*/
 
-
+//ques 12 . count no. of zeroes in a number
+/*class Main {
+    public static int countzeroes(int n)
+    {
+        return helper(n,0);
+    }
+    
+    public static int helper(int n,int c)
+    {
+        if(n==0) return c;
+        int rem=n%10;
+        if(rem==0)
+        {
+            return helper(n/10,c+1);
+        }
+        else{
+            return helper(n/10,c);
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.print(countzeroes(302040));
+    }
+}*/
 
 
 
