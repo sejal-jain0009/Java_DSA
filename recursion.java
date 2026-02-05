45045c9b229065202c1f393232067b3f67578779
Sejal jain <sejaljain0009@gmail.com>
2026-02-05 12:17:23 +0530

    palindrome

--- recursion.java
+++ recursion.java
@@ -166,7 +166,31 @@
     }
 }*/
 
-
+//ques 11 . palindrome of a number
+/*class Main {
+    static int sum=0;
+    public static int reverse(int n){
+        //Sometimes you might need some different variables in the arguments in that case, make another function
+    int digits=(int)(Math.log10(n))+1;//to find no. of digits in a no.
+        return helper(n,digits);
+    }
+    
+    public static int helper(int n,int digits)
+    {
+        if(n%10==n) return n;//means if it is a single digit number
+        int rem=n%10;
+        return rem*(int)Math.pow(10,digits-1)+helper(n/10,digits-1);
+    }
+    
+    public static boolean palin(int n)
+    {
+        return n==reverse(n);
+    }
+    
+    public static void main(String[] args) {
+        System.out.print(palin(1234321));
+    }
+}*/
 
 
 
