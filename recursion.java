732ff92c5859c04f08f4af1b1002cf5a29ab78cf
Sejal jain <sejaljain0009@gmail.com>
2026-02-12 13:25:43 +0530

    merge sort using recursion

--- recursion.java
+++ recursion.java
@@ -456,3 +456,66 @@ class Main {
        selection(arr,start+1);
     }
 }*/
+
+//ques 24 : merge sort using recursion
+/*import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       int[] arr={8,3,4,12,5,6};
+       arr=mergeSort(arr);
+       System.out.print(Arrays.toString(arr));
+    }
+    
+    public static int[] mergeSort(int[] arr){
+       if(arr.length==1) return arr;
+       
+       int mid=arr.length/2;
+       int[] left= Arrays.copyOfRange(arr,0,mid);
+       int[] right= Arrays.copyOfRange(arr,mid,arr.length);
+       
+       left=mergeSort(left);
+       right=mergeSort(right);
+       return merge(left,right);
+    }
+    
+    public static int[] merge(int[] left,int[] right)
+    {
+        int[] mix=new int[left.length+right.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<left.length && j<right.length)
+        {
+            if(left[i]<right[j])
+            {
+                mix[k]=left[i];
+                i++;
+            }
+            else {
+                mix[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+        
+        //It may be possible that one of array will reach till end and elements of other arrays  are not inserted in mix array completely
+        
+        while(i<left.length)
+        {
+            mix[k]=left[i];
+            i++;
+            k++;
+        }
+        
+        while(j<right.length)
+        {
+            mix[k]=right[j];
+            j++;
+            k++;
+        }
+        
+        //either one of the above loop will be true
+        
+        return mix;
+    }
+}*/
