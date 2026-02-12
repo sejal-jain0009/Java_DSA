328b0e468b9155a519baef769e0d4d3be235cd4c
Sejal jain <sejaljain0009@gmail.com>
2026-02-12 10:06:42 +0530

    bubble sort using recursion

--- recursion.java
+++ recursion.java
@@ -402,3 +402,29 @@ public static void backTrackingPermutation(StringBuilder sb,int start)
         }
     }
 }*/
+
+//ques 23 : Buuble sort using recursion
+/*import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int[] arr={9,6,0,4,3};
+       bubble(arr,arr.length-1,0);
+       System.out.println(Arrays.toString(arr));
+    }
+    public static void bubble(int[] arr,int row,int col){
+        if(row==0) return;
+        if(col<row){
+            
+        if(arr[col]>arr[col+1])
+        {
+            int temp=arr[col];
+            arr[col]=arr[col+1];
+            arr[col+1]=temp;
+        }
+        bubble(arr,row,col+1);
+        }
+        else{
+            bubble(arr,row-1,0);
+        }
+    }
+}*/
