678019bb088acec70532c1a7e56d14cfa0c0a1b6
Sejal jain <sejaljain0009@gmail.com>
2026-02-22 13:27:30 +0530

    to check if no. is even or odd using bit masking

---
+++ bitmasking.java
@@ -0,0 +1,10 @@
+//ques - to check even odd using bitmasking
+/*class Main
+{
+	public static void main(String[] args) {
+		int n1=7;
+		int n2=8;
+		if((n2 & 1)==1) System.out.println("Odd");
+		else System.out.println("Even");
+	}
+}*/
