599e5777c09157a33dcd0ed84f6d935e26a11216
Sejal jain <sejaljain0009@gmail.com>
2026-02-22 13:39:45 +0530

    implementation of bit masking of all operations

--- bitmasking.java
+++ bitmasking.java
@@ -8,3 +8,18 @@
 		else System.out.println("Even");
 	}
 }*/
+
+//ques- implemention of operations of bit masking
+/*class Main
+{
+	public static void main(String[] args) {
+		int n1=5;
+		int n2=3;
+		System.out.println(5 & 3); //And operation
+		System.out.println(5 | 3); //or operation
+		System.out.println(5 ^ 3); //Xor operation
+		System.out.println(~5); //Not operation
+		System.out.println(5<<1); //left - multiply by 2
+		System.out.println(5>>1);//right - divide by 2
+	}
+}*/
