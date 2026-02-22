b294d4fa43493f3f8e737b6c37c333e5dc442909
Sejal jain <sejaljain0009@gmail.com>
2026-02-22 16:43:34 +0530

    power set of set using bit masking

--- bitmasking.java
+++ bitmasking.java
@@ -98,3 +98,25 @@
         System.out.print(ans);
     }
 }*/
+
+//ques - power set
+/*class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = {1,2,3};
+        int n = arr.length;
+
+        for(int i=0;i<(1<<n);i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if((i&(1<<j))!=0){
+                    System.out.print(arr[j]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
