b48018f96101c4c051258324c9c3ff7e0c6de71f
Sejal jain <sejaljain0009@gmail.com>
2026-02-24 17:40:56 +0530

    Create a class Car with: brand and price and method display() Then create 2 objects

--- oops.java
+++ oops.java
@@ -1,7 +1,5 @@
 //ques - syntax of class
-/*class Student{
-    String name="sejal";
-    int rollno=1519;
+/*class Student{ 
     void display(){
         System.out.println("Name is : "+name+"\nRollno. is : "+rollno);
     }
@@ -9,7 +7,33 @@
 public class oops
 {
 	public static void main(String[] args) {
-	    Student s1=new Student();
+	    Student s1=new Student();//object creation
+        s1.name="Rohit";
+        s1.rollno=101;
 		s1.display();
+	}
+}*/
+
+//ques - Create a class Car with: brand and price and method display() Then create 2 objects.
+/*class Car{
+    String brand;
+    double price;
+    void display(){
+        System.out.println("Brand is : "+brand+"\nPrice is : "+price);
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	    Car c1=new Car();
+		c1.brand="tata";
+		c1.price=100.8;
+		
+		Car c2=new Car();
+		c2.brand="gchd";
+		c2.price=97749.89;
+		
+		c1.display();
+		c2.display();
 	}
 }*/
